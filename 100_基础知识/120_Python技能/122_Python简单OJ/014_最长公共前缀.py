b84fc84ecc36eee5e399e42ca58f47fad08e632b
刘蘩 <liufan.dery@gmail.com>
2020-06-19 15:07:00 +0800

    Update 014_最长公共前缀.py

--- 100_基础知识/120_Python技能/122_Python简单OJ/014_最长公共前缀.py
+++ 100_基础知识/120_Python技能/122_Python简单OJ/014_最长公共前缀.py
@@ -1,14 +1,12 @@
 """
 编写一个函数来查找字符串数组中的最长公共前缀。
-
 如果不存在公共前缀，返回空字符串 ""。
 
 示例 1:
-
 输入: ["flower","flow","flight"]
 输出: "fl"
-示例 2:
 
+示例 2:
 输入: ["dog","racecar","car"]
 输出: ""
 解释: 输入不存在公共前缀。
@@ -16,3 +14,48 @@
 
 所有输入只包含小写字母 a-z 。
 """
+# 取每一个单词的同一位置的字母，看是否相同
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        res = ""
+        for tmp in zip(*strs):
+            tmp_set = set(tmp)
+            if len(tmp_set) == 1:
+                res += tmp[0]
+            else:
+                break
+        return res
+
+# 取一个单词 s，和后面单词比较，看 s 与每个单词相同的最长前缀是多少！遍历所有单词。
+class Solution:
+    def longestCommonPrefix(self, s: List[str]) -> str:
+        if not s:
+            return ""
+        res = s[0]
+        i = 1
+        while i < len(s):
+            while s[i].find(res) != 0:
+                res = res[0:len(res)-1]
+            i += 1
+        return res
+# 思路 3：
+# 按字典排序数组，比较第一个，和最后一个单词，有多少前缀相同。
+class Solution:
+    def longestCommonPrefix(self, s: List[str]) -> str:
+        if not s:
+            return ""
+        s.sort()
+        n = len(s)
+        a = s[0]
+        b = s[n-1]
+        res = ""
+        for i in range(len(a)):
+            if i < len(b) and a[i] == b[i]:
+                res += a[i]
+            else:
+                break
+        return res
